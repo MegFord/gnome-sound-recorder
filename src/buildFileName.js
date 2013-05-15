8d2fa3c9d4d031a6d2d6299f57bbdf72612ffbdb
Meg Ford <meg387@gmail.com>
2013-05-15 13:55:04 -0500

    Add buildFileName.js

---
+++ src/buildFileName.js
@@ -0,0 +1,41 @@
+/*
+ *  Author: Meg Ford <megford@gnome.org>
+ *
+ * Copyright 2013 Meg Ford
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ *
+ */
+ 
+imports.gi.versions.Gst = '0.10';
+
+const Gst = imports.gi.Gst;
+
+const Record = imports.record;
+
+const buildFileName = new Lang.Class({
+    Name: 'BuildFileName',
+
+    _expandInitialTilde: function() {
+        //if (path[1] == "/" || path[1] == "\\0") {
+       // }
+    },
+
+    _buildDefaultFilename: function() {
+        
+    
+    }
+});
