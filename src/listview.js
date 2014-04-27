34816f749989eb990f857817bc2b4e17f9f108eb
Meg Ford <megford@gnome.org>
2014-04-26 20:04:32 -0500

    listview.js: remove bad paste

--- src/listview.js
+++ src/listview.js
@@ -10,7 +10,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Library General Public License for more details.
  *
- * You should have received a copy of the GNU Library General PublicGstPbutils.pb_utils_get_codec_description
+ * You should have received a copy of the GNU Library General Public
  * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  *
  *
